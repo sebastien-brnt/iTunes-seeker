88f9c4e7d3e5c8404c3060773c27df6be18dc191
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-04-11 20:10:16 +0100

    Custom header titles for screens

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text } from 'react-native';
 
 // Import des pages
 import HomeScreen from './components/HomeScreen';
@@ -7,16 +8,37 @@ import PlaylistScreen from './components/PlaylistScreen';
 import SearchResultScreen from './components/SearchResultScreen';
 
 export default function App() {
-  const Stack = createNativeStackNavigator() ; // Permet de définit la pile de navigation
+  const Stack = createNativeStackNavigator(); // Permet de définir la pile de navigation
 
   return (
-    // Logique de navigation dans l'application
+    // Logique de navigation de l'application :
+    //    - 3 écrans : HomeScreen, PlaylistScreen, SearchResultScreen
+    //    - Ecran de démarrage : HomeScreen
+
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-        <Stack.Screen name="Playlist" component={PlaylistScreen}></Stack.Screen>
-        <Stack.Screen name="SearchResult" component={SearchResultScreen}></Stack.Screen>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{ headerTitle: props => <Text style={styles.title}>Accueil</Text> }}></Stack.Screen>
+
+        <Stack.Screen 
+          name="Playlist" 
+          component={PlaylistScreen}
+          options={{ headerTitle: props => <Text style={styles.title}>Ma playlist</Text> }}></Stack.Screen>
+
+        <Stack.Screen 
+          name="SearchResult" 
+          component={SearchResultScreen}
+          options={{ headerTitle: props => <Text style={styles.title}>Résultat de recherche</Text> }}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
