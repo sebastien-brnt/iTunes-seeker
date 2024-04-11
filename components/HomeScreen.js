5ff5cfac6acee0a96bc166a0a9cf570ce8a0f010
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-04-11 19:04:20 +0100

    Add search bar in home

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,9 +1,29 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View, StyleSheet, Button, TextInput } from "react-native"
+import { useState } from "react";
 
 export default function HomeScreen({navigation}) {
+    // Variables spécifiques à la recherche
+    const [search, setSearch] = useState('');
+
     return (
         <View style={styles.container}>
-            <Text>My Homepage</Text>
+                <Text>Recherche</Text>
+
+                {/* Barre de recherche */}
+                <View style={styles.rowInput}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setSearch}
+                        value={search}
+                        placeholder="Rechercher un morceaux, artiste, album..."
+                        keyboardType="default"
+                        onSubmitEditing={() => navigation.navigate('SearchResult', {search})}
+                    />
+                    <Button title="Rechercher" onPress={() => navigation.navigate('SearchResult', {search})}></Button>
+                </View>
+
+                {/* Accès à la playlist */}
+                <Button title="Ma playlist" onPress={() => navigation.navigate('Playlist')}></Button>
         </View>
     )
 }
@@ -15,4 +35,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    rowInput: {
+        width: '90%',
+        marginTop: 20,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        flex: 1,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#e1e1e1'
+    }
 });
