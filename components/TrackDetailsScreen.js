1b4d37552f66b9606082f839073214e53e1ff1ed
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-04-18 19:31:41 +0100

    Clarification of the TrackDetailsScreen

--- components/TrackDetailsScreen.js
+++ components/TrackDetailsScreen.js
@@ -1,35 +1,37 @@
 import { Text, View, Image, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { trackExists } from "./TracksSlice";
-import AddRating from "./AddRating";
 import AddTrack from "./AddTrack";
 import RemoveTrack from "./RemoveTrack";
 import { ratingSelectorById } from "./RatingsSlice";
-import RatingDisplay from "./RatingDisplay";
+import AddRating from "./AddRating";
 import RemoveRating from "./RemoveRating";
+import RatingDisplay from "./RatingDisplay";
 
 export default function TrackDetailsScreen({ route }) {
     // Récupération des informations du morceau
     const { track } = route.params;
-    let trackRating = undefined;
+    
+    // Récupération de la note du morceau si il est dans la liste des morceaux noté
+    const trackRating = useSelector(state => ratingSelectorById(state, track.trackId));;
 
     // Vériication de l'existence du morceau dans la playlist
     const isTrackPresent = useSelector(state => trackExists(state, track.trackId));
 
-    // Récupération de la note du morceau si il est dans la liste des morceaux noté
-    trackRating = useSelector(state => ratingSelectorById(state, track.trackId));
-    
     // Fonction pour formater le temps en minutes et secondes
-    function formatTime(milliseconds) {
+    const formatTime = milliseconds => {
         const totalSeconds = Math.floor(milliseconds / 1000);
         const minutes = Math.floor(totalSeconds / 60);
         const seconds = totalSeconds % 60;
         return `${minutes}min ${seconds < 10 ? '0' : ''}${seconds}s`;
+    };
+
+    if (!track) {
+        return <Text style={styles.noResult}>Aucune information sur le morceau disponible.</Text>
     }
 
     return (
         <View style={styles.container}>
-            {track ? (
                 <View>
                     {/* Header du morceau */}
                     <View style={styles.header}>
@@ -41,13 +43,9 @@ export default function TrackDetailsScreen({ route }) {
                             <Text style={styles.trackName}>{track.trackName}</Text>
                             <Text style={styles.artistName}>{track.artistName}</Text>
 
-                            {/* Boutons pour ajouter / retirer le morceau à la playlist */}
+                            {/* Boutons pour ajouter / retirer le morceau de la playlist en fonction de sa présence */}
                             <View style={styles.playlistButton}>
-                                {isTrackPresent ? (
-                                    <RemoveTrack track={track}/>
-                                ) : (
-                                    <AddTrack track={track}/>
-                                )}
+                                {isTrackPresent ? <RemoveTrack track={track}/> : <AddTrack track={track}/> }
                             </View>
                         </View>
                     </View>
@@ -60,7 +58,7 @@ export default function TrackDetailsScreen({ route }) {
                     <Text style={styles.detail}>Type de musique : {track.primaryGenreName}</Text>
                     <Text style={styles.detail}>Album : {track.collectionName}</Text>
 
-                    {/* Notation du morceau */}
+                    {/* Affichage de la note avec la possibilité de la supprimer ou du système de notation suivant si l'utilisateur a noté le morceau*/}
                     <Text style={styles.title}>Notation</Text>
                     {trackRating ? 
                         <View>
@@ -72,9 +70,6 @@ export default function TrackDetailsScreen({ route }) {
                         <AddRating track={track} />
                     }
                 </View>
-            ) : (
-                <Text style={styles.title}>Aucune information sur le morceau disponible.</Text>
-            )}
         </View>
     )
 }
@@ -115,5 +110,9 @@ const styles = StyleSheet.create({
     },
     detail: {
         marginBottom: 5
+    },
+    noResult: {
+        marginTop: 20,
+        textAlign: 'center'
     }
 });
