7f1abdf28d91250bbbc8b5411afcfae75453e0e9
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-04-18 20:27:28 +0100

    Clairification of the PlaylistScreen page

--- components/PlaylistScreen.js
+++ components/PlaylistScreen.js
@@ -3,25 +3,27 @@ import TrackItem from "./TrackItem";
 import { useSelector } from "react-redux";
 import { trackSelector } from "./TracksSlice";
 
-export default function OtherScreen({route, navigation}) {
+export default function PlaylistScreen() {
     // Récupération des morceaux de la playlist
     const tracks = useSelector(trackSelector);
 
+    // Affichage d'un message si aucun morceau n'a été noté
+    if (tracks.length === 0) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.textEmptyPlaylist}>Votre playlist ne contient aucun morceau pour le moment.</Text>
+            </View>
+        );
+    }
+
+    // Affichage de la liste des morceaux de la playlist
     return (
         <View style={styles.container}>
-            {/* Liste des morceau de la playlist */}
-            {tracks.length >= 1 ? (
-                <FlatList
+            <FlatList
                 data={tracks}
                 keyExtractor={(item) => item.trackId ? item.trackId.toString() : item.collectionId.toString()}
-                renderItem={({ item }) => (
-                    // Utilisation du composant TrackItem pour afficher les informations du morceau
-                    <TrackItem track={item} />
-                )}/>
-            ) : (
-                // Message si aucun résultat n'est trouvé
-                <Text style={styles.emptyPlaylist}>Votre playlist ne contient aucun morceau pour le moment.</Text>
-            )}
+                renderItem={({ item }) => ( <TrackItem track={item} /> )}  // Utilisation du composant TrackItem pour afficher les informations du morceau
+            />
         </View>
     )
 }
@@ -32,7 +34,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         padding: 20,
     },
-    emptyPlaylist: {
+    textEmptyPlaylist: {
         marginTop: 50,
         textAlign: 'center',
     }
