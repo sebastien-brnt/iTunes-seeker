63f431689acc02e2d56c853e8d15f31bd5a51d05
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-04-18 19:41:14 +0100

    Clarification of SearchResultScreen

--- components/SearchResultScreen.js
+++ components/SearchResultScreen.js
@@ -2,28 +2,36 @@ import { Text, View, StyleSheet, ActivityIndicator, FlatList } from "react-nativ
 import { useState, useEffect } from "react";
 import TrackItem from "./TrackItem";
 
-export default function SearchResultScreen({ route, navigation }) {
+export default function SearchResultScreen({ route }) {
   const { search } = route.params;
   const [loading, setLoading] = useState(true);
   const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    // Fonction pour effectuer une recherche avec l'API iTunes
-    const searchWithAPI = async (term) => {
-      try {
-        const response = await fetch(
-          `https://itunes.apple.com/search?term=${encodeURIComponent(term)}`
-        );
-        const data = await response.json();
-        setResults(data.results);
-        setLoading(false);
-      } catch (error) {
-        console.error("Erreur lors de la recherche :", error);
-        setLoading(false);
-      }
-    };
+  // Fonction pour effectuer une recherche avec l'API iTunes
+  const searchWithAPI = async (term) => {
+    if (!term) {
+      setLoading(false);
+      return;
+    }
 
-    // Lancement de la recherche
+    try {
+      const response = await fetch(
+        `https://itunes.apple.com/search?term=${encodeURIComponent(term)}`
+      );
+      const data = await response.json();
+      if (data.resultCount === 0) {
+        setError('Aucun résultat trouvé.');
+      } else {
+        setResults(data.results);
+      }
+    } catch (error) {
+      setError(`Erreur lors de la recherche : ${error.message}`);
+    }
+    setLoading(false);
+  };
+
+  // Lancement de la recherche
+  useEffect(() => {
     searchWithAPI(search);
   }, [search]);
 
@@ -38,29 +46,27 @@ export default function SearchResultScreen({ route, navigation }) {
     );
   }
 
-  // Affichage du résultat de la recherche lorsque le chargement est terminé
+  // Affichage d'un message si aucun résultat n'est trouvé
+  if (results.length <= 0) {
+    return (
+      <View style={styles.container}>
+        <Text>Aucun résultat trouvé.</Text>
+      </View>
+    );
+  }
+
+  // Affichage du résultat de la recherche lorsque le chargement est terminé et qu'il y a des résultats
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Votre recherche : {search}</Text>
-  
       {/* Affichage du résultat de la recherche uniquement si il y a des résultats */}
-      {results.length > 0 ? (
-        <FlatList
-          style={styles.list}
-          data={results}
-          keyExtractor={(item) => item.trackId ? item.trackId.toString() : item.collectionId.toString()}
-          renderItem={({ item }) => (
-            // Utilisation du composant TrackItem pour afficher les informations du morceau
-            <TrackItem track={item} />
-          )}
-        />
-      ) : (
-        // Message si aucun résultat n'est trouvé
-        <Text>Aucun résultat trouvé.</Text>
-      )}
+      <FlatList
+        style={styles.list}
+        data={results}
+        keyExtractor={(item) => item.trackId.toString()}
+        renderItem={({ item }) => <TrackItem track={item} />} // Utilisation du composant TrackItem pour afficher les informations du morceau
+      />
     </View>
   );
-  
 }
 
 const styles = StyleSheet.create({
